a9e21ae5fc9ca7cee4b7ff2dcefb62c2fe713abe
mikeyao <yaojunmike@gmail.com>
2015-03-27 21:18:34 -0700

    Hash table extra credit done

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,56 +1,91 @@
 var HashTable = function(){
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-  for (var i = 0; i < this._limit; i++){
-    //this._storage.set(i, {});
-    this._storage.set(i, []);
-  }
+  this._bucketSize = 1;
+  this.reset(this._limit);
 };
+
+HashTable.prototype.reset = function(limit){
+  this._counter = 0;
+  this._storage = LimitedArray(limit);
+  for (var i = 0; i < limit; i++){
+    this._storage.set(i, {});
+    // this._storage.set(i, []);
+  }
+}
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var obj = this._storage.get(i);
-  //obj[k] = v;
-  obj.push({"key": k, "value": v});
+  if (!obj.hasOwnProperty(k)){
+    this._counter++;
+  }
+  obj[k] = v;
+  if (this._counter >= 0.75 * this._limit){
+      this.resize(this._limit * 2);
+  }
+  // obj.push({"key": k, "value": v});
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var obj = this._storage.get(i);
-  // return obj.hasOwnProperty(k) ? obj[k] : null;
-  var result = null;
-  _.each(obj, function(tuple){
-    if (tuple["key"] === k){
-      result = tuple["value"];
-    }
-  });
+  return obj.hasOwnProperty(k) ? obj[k] : null;
+  // var result = null;
+  // _.each(obj, function(tuple){
+  //   if (tuple["key"] === k){
+  //     result = tuple["value"];
+  //   }
+  // });
   // for (var i = 0; i < obj.length; i++){
   //   if (obj[i]["key"] === k){
   //     result = obj[i]["value"];
   //     return result;
   //   }
   // }
-  return result;
+  // return result;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var obj = this._storage.get(i);
-  // delete obj[k];
-  var id = null;
-  id = _.each(obj, function(tuple, index){
-    if (tuple["key"] === k){
-      return index;
-    }
-  });
-  if (id !== null){
-    obj.splice(id,1);
+  if (obj.hasOwnProperty(k)){
+    this._counter--;
   }
+  delete obj[k];
+  if (this._counter < 0.25 * this._limit){
+    // debugger;
+    this.resize(0.5 * this._limit);
+  }
+  // var id = null;
+  // id = _.each(obj, function(tuple, index){
+  //   if (tuple["key"] === k){
+  //     return index;
+  //   }
+  // });
+  // if (id !== null){
+  //   obj.splice(id,1);
+  // }
 };
 
+HashTable.prototype.resize = function(newLimit){
+  //takeout
+  var tempStorage = {};
+  for (var i = 0; i < this._limit; i++){
+    var obj = this._storage.get(i);
+    for (var key in obj){
+      tempStorage[key] = obj[key];
+    }
+  }
 
+  this._limit = newLimit;
+  this.reset(newLimit);
 
-// Object.keys(obj).length == this._limit;
+  //add it back
+  for (var key in tempStorage){
+    this.insert(key, tempStorage[key]);
+    // this.insert.bind(this, ?)
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
